bac4325123aa8de80d20c6b7557ad067b7d5a66d
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2024-12-12 14:53:04 +0700

    webservice berhasil

--- WebService/json_konversi.php
+++ WebService/json_konversi.php
@@ -1,4 +1,4 @@
-. <?php
+<?php
 include "koneksi.php";
 
 $link = koneksi_db();
--- WebService/json_parsing.php
+++ WebService/json_parsing.php
@@ -3,7 +3,6 @@
 $file = file_get_contents('http://localhost/Matkul-Penerapan-Teknologi-Internet/WebService/json_konversi.php');
 $json = json_decode($file, true);
 
-var_dump($json);
 foreach ($json as $key) {
     if (is_array($key)) {
         foreach ($key as $key => $value) {
