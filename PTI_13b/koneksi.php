31785290cc7e3d07945a9526d8f5dcd3a02620c9
aprnna <aprianacahya04@gmail.com>
2025-01-16 13:14:45 +0700

    foldering

--- PTI_13b/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "crud_mapbox";
-
-$connect = mysqli_connect($host, $username, $password, $database); // Koneksi ke MySQL
